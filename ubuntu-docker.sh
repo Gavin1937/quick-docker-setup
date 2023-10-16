b1cd6197a7fb26d0d2887bd7f238d4a7434a095c
Gavin1937 <71205842+Gavin1937@users.noreply.github.com>
2023-10-15 23:01:56 -0700

    Create ubuntu-docker.sh

---
+++ ubuntu-docker.sh
@@ -0,0 +1,19 @@
+#! /bin/bash -x
+
+# Add Docker's official GPG key:
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update -y
+
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose
+
+sudo docker run hello-world
